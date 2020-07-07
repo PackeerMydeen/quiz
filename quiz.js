318fe4925a71479e18843266bb5731c655e784a0
Packeer Mydeen <packeer@Packeers-MacBook-Pro.local>
2020-07-07 19:53:53 +0530

    Initialize project using Create React App

---
+++ quiz.js
@@ -0,0 +1,17 @@
+export const questionBook=[
+    {
+        question:'How are you',
+        options:['a','b','c'],
+        ans:'b'
+    },
+    {
+        question:'How are you',
+        options:['a','b','c'],
+        ans:'b'
+    },
+    {
+        question:'How are you',
+        options:['a','b','c'],
+        ans:'b'
+    }
+]
